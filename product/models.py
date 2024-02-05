7158740d2aed9aad55e626715d4d4514cd669079
Bluiss <135335614+Bluiss@users.noreply.github.com>
2024-02-05 20:47:52 +0000

    add product model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
+
+class Product(models.Model):
+    seller = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
