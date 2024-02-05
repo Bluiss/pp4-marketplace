7c46b65f05b4103fe2c577dc2609355d63816b17
Bluiss <135335614+Bluiss@users.noreply.github.com>
2024-02-05 21:30:24 +0000

    add product html and create paths for product page

--- core/urls.py
+++ core/urls.py
@@ -15,14 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from accounts.views import accounts
-from product.views import product
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', accounts, name='accounts'),
-    path('product/', product, name='product'),
-
+    path('product/', include('product.urls')), 
 ]
+
