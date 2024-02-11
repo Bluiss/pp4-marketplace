661b3c9900e26a06dd1dd1d59de01ee81f1a431d
Bluiss <135335614+Bluiss@users.noreply.github.com>
2024-02-11 20:26:52 +0000

    merge branches and fix issues with all auth, add allauth templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView  # Add this import
+from django.views import View
+
+
 
 # Create your views here.
 class RegisterPageView(TemplateView): 
