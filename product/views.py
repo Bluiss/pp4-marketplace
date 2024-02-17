237ab9dbdbce1a5e93ab1d2bafa93c576be8446d
Bluiss <135335614+Bluiss@users.noreply.github.com>
2024-02-17 16:52:33 +0000

    correct views function to post new product to datbase

--- product/views.py
+++ product/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
+from django.contrib import messages
+
 
 from .models import Product
 from .forms import NewProductForm
@@ -16,7 +18,16 @@ class ProductListDetail(generic.DetailView):
 
 @login_required
 def new(request):
-    # Instantiate the form
     form = NewProductForm()
+
+    if request.method == "POST":
+        newForm = NewProductForm(request.POST, request.FILES)  
+        if newForm.is_valid():
+            new_product = newForm.save(commit=False)
+            new_product.seller = request.user
+            new_product.save()
+
+            messages.success(request, "Product uploaded")
+            return redirect('product:product_detail', pk=new_product.pk)
 
     return render(request, 'product/form.html', {'form': form})
