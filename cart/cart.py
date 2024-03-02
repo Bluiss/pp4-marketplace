2f4b362c4b370a3377744eba73104cfca937e0d1
Bluiss <135335614+Bluiss@users.noreply.github.com>
2024-03-02 23:06:16 +0000

    add cart_add to views.py , cart.py and add JS to product_detail.html

--- cart/cart.py
+++ cart/cart.py
@@ -11,3 +11,14 @@ class Cart():
 
         # make cart available on all pages
         self.cart = cart
+
+
+    def add(self, product):
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': product.price}
+
+        self.session = True
--- cart/views.py
+++ cart/views.py
@@ -1,17 +1,29 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from product.models import Product
+from django.http import JsonResponse
 
 def cart_summary(request):
     return render(request, "cart_summary.html", {})
 
 def cart_add(request):
-    pass
+    # get cart
+    cart = Cart(request)
+    
+    # test post
+    if request.POST.get('action') == 'post':
+        product_id = request.POST.get('product_id')
+        if product_id is None:
+            return JsonResponse({'error': 'No product_id in POST data'}, status=400)
+        product_id = int(product_id)
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product)
 
+        response = JsonResponse({'Product Name': product.title})  # change 'name' to 'title'
+        return response
 
 def cart_delete(request):
     pass
-
 
 def cart_update(request):
     pass
