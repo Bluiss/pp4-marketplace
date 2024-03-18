90915d621e1763615e199101abfe3a48addf9278
Bluiss <135335614+Bluiss@users.noreply.github.com>
2024-03-18 20:01:44 +0000

    style navbar

--- product/views.py
+++ product/views.py
@@ -32,26 +32,18 @@ class ProductListDetail(generic.DetailView):
 # View for creating a new product (requires user authentication)
 @login_required
 def new(request):
-    # Instantiate a new product form
     form = ProductForm()
 
-    # Check if the form has been submitted
     if request.method == "POST":
-        # Create a new form instance with the submitted data and files
-        newForm = ProductForm(request.POST, request.FILES)
-        
-        # Check if the form is valid
+        newForm = ProductForm(request.POST, request.FILES)  
         if newForm.is_valid():
-            # Create and save a new product with the current user as the seller
             new_product = newForm.save(commit=False)
             new_product.seller = request.user
             new_product.save()
 
-            # Display a success message and redirect to the new product's detail page
             messages.success(request, "Product uploaded")
             return redirect('product:product_detail', pk=new_product.pk)
 
-    # Render the form template with the instantiated form``
     return render(request, 'product/new.html', {'form': form})
 
 
