ef7e4c53e325b3161c5f75798dfaf52884f42fb2
Bluiss <135335614+Bluiss@users.noreply.github.com>
2024-03-21 20:32:24 +0000

    add product_cart model

--- product_cart/models.py
+++ product_cart/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from .models import Product 
 
 # Create your models here.
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product) 
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"Cart for {self.user.username}"
+
+    def get_absolute_url(self):
+        return reverse("cart:cart_detail")
