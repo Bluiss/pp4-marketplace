49c7736a634080d2aa3256a837041b5e4d055b1b
Bluiss <135335614+Bluiss@users.noreply.github.com>
2024-03-21 20:47:51 +0000

    add add/delete/detail views to cart app

--- product_cart/views.py
+++ product_cart/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Cart 
+from .models import Product
 
 # Create your views here.
+# Add cart view
+@login_required
+def add_cart(request, product_id):
+    cart_product = get_object_or_404(Product, id=product_id)
+
+    if cart_product:
+        cart_product.quantity += 1
+        cart_product.save()
+        messages.success(request, "Product added to cart")
+    else:
+        cart_product = Product.objects.create(product=cart_product)
+        cart_product.save()
+        messages.success(request, "Product added to cart")
+
+    return redirect("cart:cart_detail")
+
+
+def delete_cart(request, product_id):
+    cart_product = get_object_or_404(Product, id=product_id)
+
+    if cart_product:
+        cart_product.quantity -= 1
+        cart_product.save()
+        messages.success(request, "Product removed from cart")
+    
+    return redirect("cart:cart_detail")
+
+
+def cart_detail(request):
+    cart = Cart.objects.filter(user=request.user)
+    total = 0
+    for product in cart:
+        total += product.quantity * product.product.price
+
+    return render(request, "cart/cart_detail.html", {"cart": cart, "total": total})
+
+    
+
+
+
+
+
+
+
+# delete cart view
+# cart detail view
+
+
+
+
