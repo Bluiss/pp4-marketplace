763fb5d658d8d1bd38f5ca1927a66daf6ec8c0f5
Harry Britton <harrydbritton@gmail.com>
2024-03-29 13:02:59 +0000

    remove environ.os from settings

--- core/settings.py
+++ core/settings.py
@@ -171,9 +171,12 @@ USE_TZ = True
 
 
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'static' 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    path.join(BASE_DIR, 'static'), 
+)
 
 
 
