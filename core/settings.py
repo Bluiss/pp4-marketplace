0ea275602a63f5eaa14a5802a67310ba2c0f7a90
Harry Britton <harrydbritton@gmail.com>
2024-03-29 13:22:26 +0000

    remove cloudinary storage

--- core/settings.py
+++ core/settings.py
@@ -172,7 +172,6 @@ USE_TZ = True
 
 
 
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
